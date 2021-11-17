7fcd311fce2ca4cc6ee9e7e8ddf211cb9ba62fc2
Antonio <tonyaguado99@gmail.com>
2021-11-17 01:02:48 +0000

    Kata 4 versión 2

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -9,11 +9,42 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
+    private List<Mail> mailList;
+    private Histogram<String> histo;
+    private HistogramDisplay histogramDisplay;
+    private String fileName;
+
+    public Main(String fileName) {
+        this.fileName = fileName;
+    }
     public static void main(String[] args) {
         String fileName = "D:\\NetBeansApplications\\Kata4\\email.txt";
         List<Mail> mailList = MailListReader.read(fileName);
         Histogram<String> histo = MailHistogramBuilder.build(mailList);;
         HistogramDisplay histogramDisplay= new HistogramDisplay(histo);
+
+        Main main = new Main("D:\\NetBeansApplications\\Kata4\\email.txt");
+        main.execute();
+
+    }
+
+    private void input(){
+        mailList = MailListReader.read(fileName);
+    }
+
+    private void process(){
+        histo = MailHistogramBuilder.build(mailList);;
+        histogramDisplay= new HistogramDisplay(histo);
+    }
+
+    private void output(){
         histogramDisplay.execute();
+    }
+
+    private void execute(){
+        input();
+        process();
+        output();
+
     }
 }
